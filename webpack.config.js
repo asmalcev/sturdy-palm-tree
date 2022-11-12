6765abd65c84003522b1506ca835d4f9c53a1352
qzerrty <qzerrty@protonmail.com>
2022-11-13 02:13:38 +0300

    ✅ + project; refactor

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,30 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  optimization: {
-    minimizer: [ new OptimizeCSSAssetsPlugin({}) ],
-  },
-  plugins: [ new MiniCssExtractPlugin({
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-  }) ],
+	mode: "development",
+	optimization: {
+		minimizer: [ new OptimizeCSSAssetsPlugin({}) ],
+	},
+	plugins: [ new MiniCssExtractPlugin({
+		filename: '[name].css',
+		chunkFilename: '[id].css',
+	}) ],
 
-  entry: './src/js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'docs'), 
-    filename: 'bundle.js' 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      }
-    ]
-  },
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve(__dirname, 'docs'), 
+		filename: 'bundle.js' 
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+					test: /\.svg$/,
+					loader: 'svg-inline-loader'
+			}
+		]
+	},
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,28 +1,6 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const devConfig = require('./webpack.config');
 
 module.exports = {
-  mode: "production", 
-  optimization: {
-    minimizer: [ new OptimizeCSSAssetsPlugin({}) ],
-  },
-  plugins: [ new MiniCssExtractPlugin({
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-  }) ],
-
-  entry: './src/js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'docs'), 
-    filename: 'bundle.js' 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      }
-    ]
-  },
+	...devConfig,
+	mode: 'production'
 }
