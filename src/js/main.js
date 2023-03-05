9e9b0dea4e970116822b286ade996dcaba08dc5f
qzerrty <qzerrty@protonmail.com>
2023-03-05 22:07:02 +0300

    refactor: убрал лишнее

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import { LightingParticles } from './LightingParticles';
 import { ScrollActivator } from './ScrollActivator';
-import { ListOfClosable } from './ListOfClosable';
 
 import '../css/index.css';
 import '../css/media.css';
@@ -13,26 +12,4 @@ window.addEventListener('load', () => {
 	const scene1Canvas = new LightingParticles('#particles', '#lighter');
 
 	const scrllAct = new ScrollActivator('scroll');
-
-	const closelst = new ListOfClosable(
-		document.querySelector('#close-list'),
-		[
-			{
-				label: 'JavaScript',
-				p: '<span class="secondary">Skills:</span> React, Webpack, Node.js, AJAX, Canvas, WebSocket'
-			},
-			{
-				label: 'Python',
-				p: '<span class="secondary">Skills:</span> Django, Uvicorn, WSGI, ASGI'
-			},
-			{
-				label: 'Other',
-				p: '<span class="secondary">Skills:</span> Git, SQL, Algorithms and Structs of data'
-			},
-			{
-				label: 'C++',
-				p: '<span class="secondary">Skills:</span> SDL1.2, SDL2'
-			},
-		]
-	);
 });
